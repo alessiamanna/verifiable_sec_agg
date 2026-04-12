073ce8dec7ed545b48c1e3e5af751189718fd31c
alessia.manna <alessia.manna@studenti.unina.it>
2026-04-12 21:56:44 +0200

    server.c refactor

--- src/server.c
+++ src/server.c
@@ -8,6 +8,7 @@
 #include "sss/sss.h"
 #include "crypto_utils.h"
 
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -24,6 +25,15 @@ static recon_ctx_t ctx_noise[MAX_NUM_CLIENTS];
 
 static recon_ctx_t ctx_mask_verif[MAX_NUM_CLIENTS]; 
 static recon_ctx_t ctx_noise_verif[MAX_NUM_CLIENTS];
+
+typedef struct{
+    recon_ctx_t* ctx_data;
+    recon_ctx_t* ctx_verif;
+
+    int db_idx_data;
+    int db_idx_verif;
+} share_target_t;
+
 
 
 // Utility to print, remove later
@@ -112,6 +122,97 @@ static bool dropout_recovered(server_t* srv){
     return true;
 }
 
+
+// functions to handle shares
+
+static bool get_share_target(server_t* srv, share_item_t* item, node_id_t target, share_target_t* out_target){
+
+    #if DEBUG
+    printf("[ROUTE] target=%d type=%d is_dropout=%d\n",
+           target,
+           item->type,
+           node_is_present(&srv->Z_set, target));
+    #endif
+
+    printf("[ROUTE] Z_set contents: ");
+for (int i = 0; i < srv->Z_set.node_count; i++)
+    printf("%d ", srv->Z_set.node_id[i]);
+printf("\n");
+
+    if(node_is_present(&srv->Z_set, target)){
+        if(item->type == SHARE_TYPE_NOISE) return false;
+        // If the target is a dropout, we need to recover both mask and noise shares to reconstruct the dropout mask
+        out_target->ctx_data = &ctx_mask[target];
+        out_target->ctx_verif = &ctx_mask_verif[target];
+        out_target->db_idx_data = DB_IDX_MASK_DATA;
+        out_target->db_idx_verif = DB_IDX_MASK_VERIF;
+    }
+    else{
+        // If the target is active, we only need to recover the noise shares to remove the noise from the aggregated sum
+        if(item->type == SHARE_TYPE_MASK) return false;
+        out_target->ctx_data = &ctx_noise[target];
+        out_target->ctx_verif = &ctx_noise_verif[target];
+        out_target->db_idx_data = DB_IDX_NOISE_DATA;
+        out_target->db_idx_verif = DB_IDX_NOISE_VERIF;
+    }
+    return true;
+}
+
+static void accumulate_share(recon_ctx_t* ctx, node_id_t helper, node_id_t target, int db_idx, uint8_t* raw_share){
+    if(ctx->done || ctx->count >= MAX_NUM_CLIENTS) return;
+    uint8_t* off = server_db_get_offset(helper, target, db_idx);
+    if(!off) return;
+    for(int j = 0; j < sss_SHARE_LEN; j++){
+        ctx->share[ctx->count][j] = raw_share[j] ^ off[j];
+    }
+    ctx->count++;
+}
+
+static void try_reconstruction(recon_ctx_t* ctx, update_t* buffer_acc){
+    if(ctx->done || ctx->count < K) return;
+    uint8_t secret_buff[sss_MLEN];
+    if(sss_combine_shares(secret_buff, ctx->share, K) == 0){
+        puf_resp_t val;
+        memcpy(&val, secret_buff, sizeof(val));
+        vector_add(buffer_acc, (update_t*)&val);
+        ctx->done = true; // if threshold shares have been reached, mark as done
+    }
+}
+
+static void compute_dropout_set(server_t* srv){
+    // This set contains a list of dropout node IDs 
+    srv->Z_set.node_count = 0;
+
+    for(int i = 0; i < srv->J_set.node_count; i++){
+        node_id_t id = srv->J_set.node_id[i];
+        // If the node is in J but not in J', it means it's a dropout
+        if(!node_is_present(&srv->J_prime_set, id)){
+            node_set_add(&srv->Z_set, id);
+        }
+    }
+
+}
+
+static void compute_global_result(update_t* out_vec, update_t* aggr, update_t* ta_mask, update_t* dropped_mask, update_t* active_noise){
+    memcpy(out_vec, aggr, sizeof(update_t)*UPDATE_LEN);
+    vector_sub(out_vec, ta_mask);
+    vector_add(out_vec, dropped_mask);
+    vector_sub(out_vec, active_noise);
+  
+}
+
+static void reset_ctx(server_t* srv){
+    memset(ctx_mask,        0, sizeof(ctx_mask));
+    memset(ctx_noise,       0, sizeof(ctx_noise));
+    memset(ctx_mask_verif,  0, sizeof(ctx_mask_verif));
+    memset(ctx_noise_verif, 0, sizeof(ctx_noise_verif));
+    memset(srv->dropped_mask_x,   0, sizeof(srv->dropped_mask_x));
+    memset(srv->droppes_mask_hat, 0, sizeof(srv->droppes_mask_hat));
+    memset(srv->active_noise_x,   0, sizeof(srv->active_noise_x));
+    memset(srv->active_noise_hat, 0, sizeof(srv->active_noise_hat));
+}
+
+
 // Run current FSM state
 void server_run_state(server_t *srv){
     if(srv->current_state){
@@ -190,24 +291,14 @@ void srv_state_req_shares(server_t* srv){
     #if DEBUG
     printf("[SERVER] Calculating dropouts");
     #endif
-
+    compute_dropout_set(srv);
     // Setup message to send to node
     srv_dropout_list_t srv_drop_msg;
     memset(&srv_drop_msg, 0, sizeof(srv_dropout_list_t));
     srv_drop_msg.type = MSG_SRV_SEND_DROP_LIST;
     srv_drop_msg.srv_id = srv->srv_id;
 
-    // This set contains a list of dropout node IDs 
-    srv->Z_set.node_count = 0;
-
-    for(int i = 0; i < srv->J_set.node_count; i++){
-        node_id_t id = srv->J_set.node_id[i];
-        // If the node is in J but not in J', it means it's a dropout
-        if(!node_is_present(&srv->J_prime_set, id)){
-            node_set_add(&srv->Z_set, id);
-        }
-    }
-
+    
     printf("[SERVER] Dropouts: %d\n", srv->Z_set.node_count);
     memcpy(&srv_drop_msg.n_3, &srv->Z_set, sizeof(node_set_t));
 
@@ -219,25 +310,13 @@ void srv_state_req_shares(server_t* srv){
     srv->io.send(srv->io.obj, (uint8_t*)&srv_drop_msg, sizeof(srv_drop_msg));
 
     // Reset context to keep track of currently received shares
-    memset(ctx_mask, 0, sizeof(ctx_mask));
-    memset(ctx_noise, 0, sizeof(ctx_noise));
-
-    memset(ctx_mask_verif, 0, sizeof(ctx_mask_verif));
-    memset(ctx_noise_verif, 0, sizeof(ctx_noise_verif));
-
-    // To store partial mask/noise aggregation results
-    memset(srv->dropped_mask_x, 0, sizeof(srv->dropped_mask_x)); 
-    memset(srv->droppes_mask_hat, 0, sizeof(srv->droppes_mask_hat)); 
-    
-    memset(srv->active_noise_x, 0, sizeof(srv->active_noise_x)); 
-    memset(srv->active_noise_hat, 0, sizeof(srv->active_noise_hat)); 
-
+    reset_ctx(srv);
     srv->current_state = srv_state_wait_recovery;
-
-
 }   
 
 // In this phase, the server waits for the shares recovered by the nodes
+
+//this can be refactored 
 void srv_state_wait_recovery(server_t* srv){
 
     node_shares_msg_t share_msg;
@@ -271,98 +350,28 @@ void srv_state_wait_recovery(server_t* srv){
         node_id_t helper = share_msg.node_id;  
         node_id_t target = item->target_node_id; 
 
-        recon_ctx_t *ctx_data = NULL;
-        recon_ctx_t *ctx_verif = NULL;
-        int db_idx_data = -1;
-        int db_idx_verif = -1;
-
-        
-        if (node_is_present(&srv->Z_set, target)) { // Recover real share for dropout mask
-            if (item->type == SHARE_TYPE_NOISE) continue; // error, the node shouldnt be in the dropout set
-
-            ctx_data = &ctx_mask[target];
-            ctx_verif = &ctx_mask_verif[target];
-            db_idx_data = DB_IDX_MASK_DATA;
-            db_idx_verif = DB_IDX_MASK_VERIF;
-        } 
-        else {
-            if (item->type == SHARE_TYPE_MASK) continue;
-
-            ctx_data = &ctx_noise[target];
-            ctx_verif = &ctx_noise_verif[target];
-            db_idx_data = DB_IDX_NOISE_DATA;
-            db_idx_verif = DB_IDX_NOISE_VERIF;
+        share_target_t share_target;
+        if(!get_share_target(srv, item, target, &share_target)){
+            #if DEBUG
+            printf("[SERVER] Invalid share type %d for target %d. Ignoring.\n", item->type, target);
+            #endif
+            continue; // skip invalid shares
         }
 
-       // If not all shares have been recovered, get the offset and compute the real share = offset ^ H_share
-        if (!ctx_data->done) {
-            uint8_t* off = server_db_get_offset(helper, target, db_idx_data);
-            if(off && ctx_data->count < MAX_NUM_CLIENTS){
-                for(int j = 0; j < sss_SHARE_LEN; j++){
-                    ctx_data->share[ctx_data->count][j] = item->share_data[j] ^ off[j];
-                }
-                ctx_data->count++;
-                
-            }
-        }
-
-        if (!ctx_verif->done) {
-            uint8_t* off = server_db_get_offset(helper, target, db_idx_verif);
-            if(off && ctx_verif->count < MAX_NUM_CLIENTS){
-                for(int j = 0; j < sss_SHARE_LEN; j++){
-                    ctx_verif->share[ctx_verif->count][j] = item->share_verif[j] ^ off[j];
-                }
-                ctx_verif->count++;
-            }
-        }
+        accumulate_share(share_target.ctx_data, helper, target, share_target.db_idx_data, item->share_data);
+        accumulate_share(share_target.ctx_verif, helper, target, share_target.db_idx_verif, item->share_verif);
     }
-
-    uint8_t secret_buf[sss_MLEN];
 
     for(int t = 0; t < MAX_NUM_CLIENTS; t++){
         // If the node is a dropout, recover shares and add them to the dropped_mask buffer
         if (node_is_present(&srv->Z_set, t)) {
-            if (!ctx_mask[t].done && ctx_mask[t].count >= K) {
-                if (sss_combine_shares(secret_buf, ctx_mask[t].share, K) == 0) {
-                    puf_resp_t val; 
-                    memcpy(&val, secret_buf, sizeof(val));
-                    vector_add(srv->dropped_mask_x, (update_t*)&val);
-                    ctx_mask[t].done = true; // if threshold shares have been reached, mark as done
-                    #if DEBUG
-                    printf("   -> Recovered Mask Data for Dropout %d\n", t);
-                    #endif
-                }
-            }
-            if (!ctx_mask_verif[t].done && ctx_mask_verif[t].count >= K) {
-                if (sss_combine_shares(secret_buf, ctx_mask_verif[t].share, K) == 0) {
-                    puf_resp_t val; 
-                    memcpy(&val, secret_buf, sizeof(val));
-                    vector_add(srv->droppes_mask_hat, (update_t*)&val); 
-                    ctx_mask_verif[t].done = true;
-                    #if DEBUG
-                    printf("   -> Recovered Mask Verif for Dropout %d\n", t);
-                    #endif
-                }
-            }
+           try_reconstruction(&ctx_mask[t], srv->dropped_mask_x);
+           try_reconstruction(&ctx_mask_verif[t], srv->droppes_mask_hat);
         }
         // Otherwise, it means it's not a dropout and only noise has to be recovered.
         else { 
-            if (!ctx_noise[t].done && ctx_noise[t].count >= K) {
-                if (sss_combine_shares(secret_buf, ctx_noise[t].share, K) == 0) {
-                    puf_resp_t val; 
-                    memcpy(&val, secret_buf, sizeof(val));
-                    vector_add(srv->active_noise_x, (update_t*)&val); // add to accumulator
-                    ctx_noise[t].done = true; // if threshold shares have been reached, mark as done
-                }
-            }
-            if (!ctx_noise_verif[t].done && ctx_noise_verif[t].count >= K) {
-                if (sss_combine_shares(secret_buf, ctx_noise_verif[t].share, K) == 0) {
-                    puf_resp_t val; 
-                    memcpy(&val, secret_buf, sizeof(val));
-                    vector_add(srv->active_noise_hat, (update_t*)&val); // add to accumulator
-                    ctx_noise_verif[t].done = true;
-                }
-            }
+                try_reconstruction(&ctx_noise[t], srv->active_noise_x);
+                try_reconstruction(&ctx_noise_verif[t], srv->active_noise_hat);
         }
     }
 
@@ -389,20 +398,13 @@ void srv_state_compute_global(server_t* srv){
     
     // Subtract masks and noise. 
     update_t final_sum_vec[UPDATE_LEN];
-    memcpy(final_sum_vec, srv->aggr_sum, sizeof(final_sum_vec)); 
-    vector_sub(final_sum_vec, srv->ta_mask_x);      
-    vector_add(final_sum_vec, srv->dropped_mask_x); 
-    vector_sub(final_sum_vec, srv->active_noise_x); 
-
     // For verification
     update_t final_verif_vec[UPDATE_LEN];
-    memcpy(final_verif_vec, srv->aggr_verif, sizeof(final_verif_vec));
-    vector_sub(final_verif_vec, srv->ta_mask_hat);       
-    vector_add(final_verif_vec, srv->droppes_mask_hat); 
-    vector_sub(final_verif_vec, srv->active_noise_hat); 
+    
+    compute_global_result(final_sum_vec, srv->aggr_sum, srv->ta_mask_x, srv->dropped_mask_x, srv->active_noise_x);
+    compute_global_result(final_verif_vec, srv->aggr_verif, srv->ta_mask_hat, srv->droppes_mask_hat, srv->active_noise_hat);
 
     payload_t payload_sum, payload_verif;
-    
     memcpy(&payload_sum, final_sum_vec, sizeof(payload_t));
     memcpy(&payload_verif, final_verif_vec, sizeof(payload_t));
 
@@ -414,7 +416,6 @@ void srv_state_compute_global(server_t* srv){
    
     sign_payload(msg.n_7, msg.n_8, srv_chain.l_7_global, msg.n_9);
     srv->io.send(srv->io.obj, (uint8_t*)&msg, sizeof(msg));
-
     printf("[SERVER] Global Update Sent. Round Complete.\n");
 
     // Update link count
