31d14c95095f0147102d5bd840e1d4c026f12526
alessia.manna <alessia.manna@studenti.unina.it>
2026-04-16 13:02:53 +0200

    update puf chain generation script

--- cr_gen.py
+++ cr_gen.py
@@ -3,17 +3,13 @@ import hashlib
 from pylfsr import LFSR
 from pypuf.simulation import XORArbiterPUF
 
-# length of the chain
 CHAIN_LEN = 100
-
-# size of the puf challenge/response
 PUF_SIZE = 128
+MAX_NUM_CLIENTS = 4
 
-# converts pypuf bits to binary
 def pypuf_to_binary(bits):
     return np.array([1 if b == 1 else 0 for b in bits], dtype=np.uint8)
 
-# converts binary bits to pypuf format
 def binary_to_pypuf(bits):
     return np.array([1 if b == 1 else -1 for b in bits], dtype=np.int8)
 
@@ -25,7 +21,6 @@ def derive_next_challenge(resp_bits):
     bits = np.array([int(b) for byte in digest[:16] for b in f"{byte:08b}"], dtype=np.uint8)
     return bits
 
-#lfsr to expand the challenge and evaluate the puf
 def lfsr_expand_and_eval(puf, challenge_bits):
     fpoly = [128, 126, 101, 99]
     lfsr = LFSR(fpoly=fpoly, initstate=challenge_bits)
@@ -37,19 +32,42 @@ def lfsr_expand_and_eval(puf, challenge_bits):
     resp_pm1 = puf.eval(real_challenges)
     return pypuf_to_binary(resp_pm1)
 
-def generate_c_array(name, puf, seed_val):
+def generate_cpp_array_ta(name, puf, seed_val):
     challenge_bits = np.array([int(b) for b in f"{seed_val:0128b}"], dtype=np.uint8)
     
-    # convert to _BitInt(128); we'll get an array. C23 needed; supported in CLANG or GCC > 14
-    c_str = f"const unsigned _BitInt(128) {name}[{CHAIN_LEN}] = {{\n"
+    c_str = f"const UInt128 {name}[MAX_NUM_CLIENTS][CHAIN_LEN] = {{\n"
+    for _ in range(MAX_NUM_CLIENTS):
+        c_str += "    {\n"
+        for _ in range(CHAIN_LEN):
+            resp_bits = lfsr_expand_and_eval(puf, challenge_bits)
+            val_int = bits_to_int(resp_bits)
+            
+            w0 = val_int & 0xFFFFFFFF
+            w1 = (val_int >> 32) & 0xFFFFFFFF
+            w2 = (val_int >> 64) & 0xFFFFFFFF
+            w3 = (val_int >> 96) & 0xFFFFFFFF
+            
+            c_str += f"        UInt128{{0x{w0:08X}, 0x{w1:08X}, 0x{w2:08X}, 0x{w3:08X}}},\n"
+            challenge_bits = derive_next_challenge(resp_bits)
+        c_str += "    },\n"
+    c_str += "};\n"
+    return c_str
+
+def generate_cpp_array_srv(name, puf, seed_val):
+    challenge_bits = np.array([int(b) for b in f"{seed_val:0128b}"], dtype=np.uint8)
     
+    c_str = f"const UInt128 {name}[CHAIN_LEN] = {{\n"
     for _ in range(CHAIN_LEN):
         resp_bits = lfsr_expand_and_eval(puf, challenge_bits)
         val_int = bits_to_int(resp_bits)
-        c_str += f"    0x{val_int:032X}uwb,\n"
         
+        w0 = val_int & 0xFFFFFFFF
+        w1 = (val_int >> 32) & 0xFFFFFFFF
+        w2 = (val_int >> 64) & 0xFFFFFFFF
+        w3 = (val_int >> 96) & 0xFFFFFFFF
+        
+        c_str += f"    UInt128{{0x{w0:08X}, 0x{w1:08X}, 0x{w2:08X}, 0x{w3:08X}}},\n"
         challenge_bits = derive_next_challenge(resp_bits)
-        
     c_str += "};\n"
     return c_str
 
@@ -60,14 +78,18 @@ if __name__ == "__main__":
     header = f"""#ifndef PUF_DATA_H
 #define PUF_DATA_H
 
-#define CHAIN_LEN {CHAIN_LEN}
+#include "common.h"
+
+// --- DEFINITIONS ---
+#define CHAIN_LEN 100
+#define MAX_NUM_CLIENTS 4
 #define PUF_SIZE_BYTE 16
 
 // ---------------- TRUSTED AUTHORITY CHAIN ----------------
-{generate_c_array("PUF_CHAIN_TA", puf_ta, seed_val=0xCAFEBABE)}
+{generate_cpp_array_ta("PUF_CHAIN_TA", puf_ta, seed_val=0xCAFEBABE)}
 
 // ---------------- SERVER CHAIN ----------------
-{generate_c_array("PUF_CHAIN_SRV", puf_srv, seed_val=0xDEADBEEF)}
+{generate_cpp_array_srv("PUF_CHAIN_SRV", puf_srv, seed_val=0xDEADBEEF)}
 
 #endif
 """
